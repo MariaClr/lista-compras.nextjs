2be3d77a1c54d00f44aa3ab87ebe2dd74094e6db
Maria Clara Oliveira <m.claraoliveiraramos@gmail.com>
2024-10-17 14:17:37 -0300

    primeiro commit

---
+++ src/app/components/nav/index.js
@@ -0,0 +1,9 @@
+export default function Nav(){
+    return (
+        <>
+            <p>Eu sou um nav</p>
+            <a>Inicio</a>
+            <a href="/sobre"></a>
+        </>
+    )
+}
---
+++ src/app/sobre/page.js
@@ -0,0 +1,7 @@
+export default function sobre(){
+    return (<>
+        <h1>Clara Maria Maria Clara</h1>
+        <p>olaolaola</p>
+        
+    </> ) 
+}
